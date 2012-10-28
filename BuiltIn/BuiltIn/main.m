8b6e61b48e93a33914cc36ca7052e6130bd698de
Alok <alok.c83@gmail.com>
2012-10-28 00:52:34 -0600

    builtIn proj

---
+++ BuiltIn/BuiltIn/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  BuiltIn
+//
+//  Created by Alix Cewall on 10/8/12.
+//  Copyright (c) 2012 AC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+     // insert code here...
+     NSLog(@"Hello, World!");
+     NSLog(@"It is awesome");
+    
+    return 0;
+}
+
