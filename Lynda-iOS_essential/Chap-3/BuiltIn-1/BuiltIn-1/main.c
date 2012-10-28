d5c4ef2aa395615d1558a27e6689aaef78cfbb55
Alok <alok.c83@gmail.com>
2012-10-28 01:02:14 -0600

    chap3

---
+++ Lynda-iOS_essential/Chap-3/BuiltIn-1/BuiltIn-1/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  BuiltIn-1
+//
+//  Created by Alix Cewall on 10/8/12.
+//  Copyright (c) 2012 AC. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
