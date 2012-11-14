6fa04bec68cf1324dbe453211645d0098a9d869a
Alok <alok.c83@gmail.com>
2012-11-13 22:40:00 -0700

    Number badge on App Icon
    - (void)applicationDidBecomeActive:(UIApplication *)application
    {
        // Restart any tasks that were paused (or not yet started) while
    the application was inactive. If the application was previously in the
    background, optionally refresh the user interface.
        // I have added this line, number on App Icon shows Chapter number
        [UIApplication sharedApplication].applicationIconBadgeNumber = 7;
    }
    
    where 7 is chapter number

--- Lynda-iOS_essential/Chap7/3_customPicker/3_customPicker/cPickerAppDelegate.m
+++ Lynda-iOS_essential/Chap7/3_customPicker/3_customPicker/cPickerAppDelegate.m
@@ -31,11 +31,14 @@
 - (void)applicationWillEnterForeground:(UIApplication *)application
 {
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+    
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application
 {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    // I have added this line, number on App Icon shows Chapter number
+    [UIApplication sharedApplication].applicationIconBadgeNumber = 7;
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application
--- Lynda-iOS_essential/Chap7/3_customPicker/3_customPicker/cPickerViewController.m
+++ Lynda-iOS_essential/Chap7/3_customPicker/3_customPicker/cPickerViewController.m
@@ -34,6 +34,7 @@
 
 - (void)viewDidLoad
 {
+    
     moods = [[NSArray alloc] initWithObjects:@"Ecstatic",@"Happy",@"Excited",@"Cheerful",@"Fine",
              @"Tired",@"Maudlin",@"Depressed",@"Overwhelmed", nil];
     
