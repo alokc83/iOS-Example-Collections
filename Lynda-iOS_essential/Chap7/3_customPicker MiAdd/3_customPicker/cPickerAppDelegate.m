0198c1422c1e00c5b33e53f646227abc139f9bfa
Alok <alok.c83@gmail.com>
2012-11-14 21:33:30 -0700

    appDelegate.m file

--- Lynda-iOS_essential/Chap7/3_customPicker MiAdd/3_customPicker/cPickerAppDelegate.m
+++ Lynda-iOS_essential/Chap7/3_customPicker MiAdd/3_customPicker/cPickerAppDelegate.m
@@ -39,6 +39,7 @@
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     // I have added this line, number on App Icon shows Chapter number
     [UIApplication sharedApplication].applicationIconBadgeNumber = 7;
+    [UIApplication sharedApplication].applicationSupportsShakeToEdit = YES;
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application
