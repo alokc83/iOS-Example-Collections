636203c02a1360027aef2273e6cf8bdfb6c4229a
Katie <katie@apples-MacBook-Pro.local>
2012-12-27 19:20:04 -0700

    Big Nerd Rach Guide book Edition 3
    First Program Quiz...

---
+++ Big-Nerd-Ranch-iOS-3Edition/Quiz/Quiz/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Quiz
+//
+//  Created by Katie on 12/27/12.
+//  Copyright (c) 2012 AC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "quizAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([quizAppDelegate class]));
+    }
+}
---
+++ Big-Nerd-Ranch-iOS-3Edition/Quiz/Quiz/quizAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  quizAppDelegate.h
+//  Quiz
+//
+//  Created by Katie on 12/27/12.
+//  Copyright (c) 2012 AC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class quizViewController;
+
+@interface quizAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) quizViewController *viewController;
+
+@end
